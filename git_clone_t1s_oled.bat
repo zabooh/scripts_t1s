3674036e01155608ef12c193ed5334d3c41026e1
zabooh <martin.ruppert@microchip.com>
2022-09-12 14:51:04 +0200

    more clean

--- git_clone_t1s_oled.bat
+++ git_clone_t1s_oled.bat
@@ -6,7 +6,7 @@ set T1S_OLED_GITHUB="https://github.com/zabooh"
 set T1S_OLED_CLIENT_INSTALL="../client"
 set T1S_OLED_SERVER_INSTALL="../server"
 
-git clone -b "v3.11.0"              %H3_GITHUB%/csp.git  %H3_INSTALL%/csp	
+git clone -b "v3.11.0"              %H3_GITHUB%/csp.git  %H3_INSTALL%/csp   
 git clone -b "v3.10.0"              %H3_GITHUB%/core.git %H3_INSTALL%/core    
 git clone -b "v3.11.1"              %H3_GITHUB%/dev_packs.git %H3_INSTALL%/dev_packs    
 git clone -b "v4.7.0"               %H3_GITHUB%/wolfssl.git %H3_INSTALL%/wolfssl
@@ -14,10 +14,10 @@ git clone -b "v3.7.4"               %H3_GITHUB%/net.git %H3_INSTALL%/net
 git clone -b "v3.7.5"               %H3_GITHUB%/crypto.git %H3_INSTALL%/crypto    
 git clone -b "v10.3.1"              %H3_GITHUB%/CMSIS-FreeRTOS.git %H3_INSTALL%/CMSIS-FreeRTOS   
 git clone -b "v1.1.0"               %H3_GITHUB%/net_10base_t1s.git %H3_INSTALL%/net_10base_t1s   
+git clone -b "v3.8.2"               %H3_GITHUB%/mhc.git %H3_INSTALL%/mhc
 
-
-git clone -b "client"				%T1S_OLED_GITHUB%/net_10base_t1s_mr.git %T1S_OLED_CLIENT_INSTALL%
-git clone -b "server"				%T1S_OLED_GITHUB%/net_10base_t1s_mr.git %T1S_OLED_SERVER_INSTALL%
+git clone -b "client"               %T1S_OLED_GITHUB%/net_10base_t1s_mr.git %T1S_OLED_CLIENT_INSTALL%
+git clone -b "server"               %T1S_OLED_GITHUB%/net_10base_t1s_mr.git %T1S_OLED_SERVER_INSTALL%
 
 pause
 @ECHO ON
